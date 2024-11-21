186f65c64aa1ccf14f3333d57870ecfc58c47c2a
MOHIT <mohitjyouti2003@gmail.com>
2024-11-21 21:56:01 +0530

    add files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
 });
 db.connect((err) => {
   if (err) {
